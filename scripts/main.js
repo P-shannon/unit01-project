9acbfc4c71fa0f381d625d90fc5c44924d93e2da
Patrick Shannon <patrickshannon05021993@gmail.com>
2017-10-08 15:52:16 -0400

    fixes #4

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,76 @@
 const main = {
-	buttClick: 0,
-	dummy: new Mob("Dum Dum",1000,1,0,0),
-	wummy: new Mob("Wum Wum",1000,1,0,0),
+	buttClick: 1,
+	dumDead: false,
 	mainWindow: document.querySelector('#main'),
+	mobs: [],
+	souls: [],
+	createMob: function(name,hp,str,agi,int){
+		this.mobs[(this.mobs.length)] = new Mob(name,hp,str,agi,int);
+	},
 	mainGameLog: function(string){
 		this.mainWindow.innerHTML += string + "<br>";
 		this.mainWindow.scrollTop = this.mainWindow.scrollHeight;
 	},
-	makeAttack: function(aggressor,defender){
+	reapSouls: function(){
+		for(let i in this.mobs){
+			if(this.mobs[i].dead){
+				this.souls[this.souls.length] = this.mobs.splice(i,1)[0];
+			}
+		}
+	},
+	makeAttack: function(aggressorName,defenderName){
+		let aggressor = null;
+		let defender = null;
+		//find the aggressor
+		for(let i in this.mobs){
+			console.log(this.mobs[i]);
+			if (this.mobs[i].name.toLowerCase() === aggressorName.toLowerCase()){
+				console.log('Found, aggressor assigned.');
+				aggressor = this.mobs[i];
+				break;
+			}
+		}
+		//find the defender
+		for(let i in this.mobs){
+			console.log(this.mobs[i]);
+			if (this.mobs[i].name.toLowerCase() === defenderName.toLowerCase()){
+				console.log("Found, defender found.");
+				defender = this.mobs[i];
+				break;
+			}
+		}
+
+		if(aggressor == null){
+			this.mainGameLog(`${aggressorName} was not found, please try again.`);
+			return false;
+		}
+		if(defender == null){
+			this.mainGameLog(`${defenderName} was not found, please try again.`);
+			return false;
+		}
+
 		let attackReport = aggressor.attack(defender);
-		this.mainGameLog(`${attackReport[0]} attacked ${attackReport[2]} dealing ${attackReport[1]} damage, leaving ${attackReport[3]}HP remaining...`);
+		if (!(attackReport[4])){
+			this.mainGameLog(`${attackReport[0]} attacked ${attackReport[2]} dealing ${attackReport[1]} damage, leaving ${attackReport[3]}HP remaining...`);
+		}
+		else {
+			this.mainGameLog(`${attackReport[0]} attacked ${attackReport[2]} dealing a killing blow, for ${attackReport[1]} damage!<br>${attackReport[0]} falls over.`);
+			this.reapSouls();
+			this.dumDead = true;
+		}
 		return attackReport;
 	},
+
+	/**********************************************
+	*
+	*  Main runtime is here. Nothing else should go 
+	*                 under here.
+	**********************************************/
+	
 	runtime: function (){
 		console.log("main.js loaded successfully.");
-		this.mainGameLog("mainWindow captured successfully.");
-		this.makeAttack(this.dummy,this.wummy);
-		//let attackReport = this.dummy.attack(this.wummy);
-		//this.mainGameLog(`${attackReport[0]} attacked ${attackReport[2]} dealing ${attackReport[1]} damage, leaving ${attackReport[3]}HP remaining...`);
+		this.createMob("Wummy",1000,100,0,0);
+		this.createMob("Dummy",1000,100,0,0);
+		this.makeAttack("Dummy","Wummy");
 	}
 }
--- scripts/mob.js
+++ scripts/mob.js
@@ -1,17 +1,26 @@
 console.log("mob.js loaded successfully");
 function Mob(name,hp,str,agi,int){
-	this.name = name;
-	this.hp = hp;
-	this.str = str;
-	this.agi = agi;
-	this.int = int;
+	this.name = name; //Identity
+	this.hp = hp;	  //Health Points
+	this.str = str;	  //Strength
+	this.agi = agi;   //Agility
+	this.int = int;   //Intelligence
+	this.dead = false;
 }
 
 Mob.prototype.takeDamage = function(damage){
 	this.hp -= damage;
+	if(this.hp <= 0){
+		this.dead = true;
+	}
+	return [this.name,this.damage,this.hp,"Mob.takeDamage"];
 }
 
 Mob.prototype.attack = function(target){
 	target.takeDamage(this.str);
-	return [this.name,this.str,target.name, target.hp];
+	let killingBlow = false;
+	if (target.hp <= 0){
+		killingBlow = true;
+	}
+	return [this.name,this.str,target.name, target.hp,killingBlow,"Mob.attack"];
 }
