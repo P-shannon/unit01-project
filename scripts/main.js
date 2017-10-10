db4f3799109250e063b9da2c84c30fcb6a506f28
Patrick Shannon <patrickshannon05021993@gmail.com>
2017-10-10 15:27:55 -0400

    gives meaning to 'AGI', fixes #10, and makes the game flow a little better.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const main = {
+const game = {
 	dumDead: false,
 	buttons: document.querySelectorAll('#buttons button'),
 	creationDialog: document.querySelector("#creation"),
@@ -8,269 +8,272 @@ const main = {
 	turnOwner: "wummy",
 	mobs: [],
 	souls: [],
-	createMob: function(name,hp,str,agi,int,controller){
-		main.mobs[(main.mobs.length)] = new Mob(name,hp,str,agi,int,controller);
+	//game.mobs[]
+	createMob: function(name, hp, str, agi, int, controller) {
+		game.mobs[(game.mobs.length)] = new Mob(name, hp, str, agi, int, controller);
 	},
-	changeCreationDialog: function(state){
-		main.creationDialog.style.visibility = state;
+	changeCreationDialog: function(state) {
+		game.creationDialog.style.visibility = state;
 	},
-	characterCreationStatMod(stat, amount){
-		if(amount < 0){
-			if(parseInt(document.querySelector(`#${stat}`).innerHTML)<=2){
+	characterCreationStatMod(stat, amount) {
+		if (amount < 0) {
+			if (parseInt(document.querySelector(`#${stat}`).innerHTML) <= 2) {
 				console.log(`${stat} too low for lowering, please don't try again`);
 				return false;
 			}
 		}
-
-		if(amount > 0){
-			if(parseInt(document.querySelector(`#cREM`).innerHTML)==0){
+		if (amount > 0) {
+			if (parseInt(document.querySelector(`#cREM`).innerHTML) == 0) {
 				console.log(`No more points left for distributing.`);
 				return false;
 			}
 		}
-
 		console.log(`Modifying ${stat} by ${amount}...`);
 		document.querySelector(`#${stat}`).innerHTML = Number(document.querySelector(`#${stat}`).innerHTML) + amount;
 		console.log(`Updating remaining points...`);
 		document.querySelector('#cREM').innerHTML = Number(document.querySelector('#cREM').innerHTML) - amount;
 	},
-	characterCreationConfirm: function(){
+	characterCreationConfirm: function() {
 		console.log(`Creating character...`)
 		let name = document.querySelector('#iName').value;
-		let hp = Number(document.querySelector('#cHP').innerHTML)*10;
+		let hp = Number(document.querySelector('#cHP').innerHTML) * 10;
 		let str = Number(document.querySelector('#cSTR').innerHTML);
 		let agi = Number(document.querySelector('#cAGI').innerHTML);
 		let int = Number(document.querySelector('#cINT').innerHTML);
-		main.createMob(name,hp,str,agi,int,false);
-		main.mainGameLog(`With a bright flash of light, your surroundings appear before you. "You know what to do, ${name}." echoes through the air briefly. <br> You have no idea what to do.`);
-		for (let i in main.mobs){
+		game.createMob(name, hp, str, agi, int, false);
+		game.mainGameLog(`With a bright flash of light, your surroundings appear before you. "You know what to do, ${name}." echoes through the air briefly. <br> You have no idea what to do.`);
+		for (let i in game.mobs) {
 			console.log("Assigning protagonist...");
 			//This is definitely going to need a sanity check.
-			if(main.mobs[i].name === name){
+			if (game.mobs[i].name === name) {
 				console.log(`Protagonist found, best of luck ${name}`);
-				main.protag = name;
-				main.mobs[i].turnTimer = 0;
+				game.protag = name;
+				game.mobs[i].turnTimer = 0;
 			}
 		}
-		main.turnOwner = main.protag;
-		main.changeCreationDialog("hidden");
-		main.showCombatants();
+		game.turnOwner = game.protag;
+		game.changeCreationDialog("hidden");
+		game.showCombatants();
 	},
-	changeButtons: function(state){
-		console.log(`Begin changing button states!`);
-		if (state === "visible"){
-			for(let i = 0; i < main.buttons.length; i++){
-				main.buttons[i].style.position = "static";
+	changeButtons: function(type,state) {
+		console.log(`Begin changing ${type} button states!`);
+		let buttons = document.querySelectorAll(`#buttons .${type}`);
+		if (state === "visible") {
+			for (let i = 0; i < buttons.length; i++) {
+				buttons[i].style.position = "static";
 			}
-		}
-		else if (state === "hidden"){
-			for(let i = 0; i < main.buttons.length; i++){
-				main.buttons[i].style.position = "absolute";
+		} else if (state === "hidden") {
+			for (let i = 0; i < buttons.length; i++) {
+				buttons[i].style.position = "absolute";
 			}
-		}
-		else{
+		} else {
 			console.log(`${state} is not a valid value, please check your code...`);
 			return false;
 		}
-		for(let i = 0; i < main.buttons.length; i++){
-			//console.log(`Changing button: ${main.buttons[i].innerHTML}'s state to: ${state}...`);
-			main.buttons[i].style.visibility = state;
+		for (let i = 0; i < buttons.length; i++) {
+			//console.log(`Changing button: ${game.buttons[i].innerHTML}'s state to: ${state}...`);
+			buttons[i].style.visibility = state;
 		}
 		console.log(`Button change complete!`);
 	},
-	clearTemps: function(){
+	clearTemps: function() {
 		let temp = document.querySelectorAll('.temp');
 		console.log(`Removing Temporary buttons.`);
-		for(let i = 0; i < temp.length; i++){
+		for (let i = 0; i < temp.length; i++) {
 			temp[i].parentElement.removeChild(temp[i]);
 		}
-		// main.changeButtons("visible");
 	},
-	giveTurn: function(){
-		let lowest = main.mobs[0].turnTimer;
+	giveTurn: function() {
+		let lowest = game.mobs[0].turnTimer;
 		let lowestAddress = 0;
 		console.log("Finding next mob...");
-		for(let i in main.mobs){
-			console.log(`Checking ${main.mobs[i].name}, Timer: ${main.mobs[i].turnTimer}`);
-			if (main.mobs[i].turnTimer < lowest){
-				lowest = main.mobs[i].turnTimer;
+		for (let i in game.mobs) {
+			console.log(`Checking ${game.mobs[i].name}, Timer: ${game.mobs[i].turnTimer}`);
+			if (game.mobs[i].turnTimer < lowest) {
+				lowest = game.mobs[i].turnTimer;
 				lowestAddress = i;
 			}
 		}
-		main.turnOwner = main.mobs[lowestAddress].name;
-		console.log(`Next up: ${main.mobs[lowestAddress].name}. Advancing turnTimers...`);
-		for(let i in main.mobs){
-			main.mobs[i].turnTimer -= lowest;
+		game.turnOwner = game.mobs[lowestAddress].name;
+		console.log(`Next up: ${game.mobs[lowestAddress].name}. Advancing turnTimers...`);
+		for (let i in game.mobs) {
+			game.mobs[i].turnTimer -= (lowest + (game.mobs[i].agi) * 4);
 		}
 		console.log(`Turn time advancement complete. Handing control to Mob.`)
-		main.controlAI(main.mobs[lowestAddress]);
+		game.controlAI(game.mobs[lowestAddress]);
 	},
-	reapSouls: function(){
-		for(let i in main.mobs){
-			if(main.mobs[i].dead){
-				if(!(main.mobs[i].cpu)){
-					main.mainGameLog("Aw jeez man, <span style='color:red'>Game Over!</span>");
+	reapSouls: function() {
+		for (let i in game.mobs) {
+			if (game.mobs[i].dead) {
+				if (!(game.mobs[i].cpu)) {
+					game.mainGameLog("Aw jeez man, <span style='color:red'>Game Over!</span>");
 				}
-				main.souls[main.souls.length] = main.mobs.splice(i,1)[0];
+				game.souls[game.souls.length] = game.mobs.splice(i, 1)[0];
 			}
 		}
 	},
 	//TODO: make this action agnostic.
-	makeAttack: function(aggressorName,defenderName){
-		main.changeButtons("hidden");
+	makeAttack: function(aggressorName, defenderName) {
+		game.changeButtons("main","hidden");
 		let aggressor = null;
 		let defender = null;
 		//find the aggressor
 		console.log(`Searching for ${aggressorName}...`);
-		for(let i in main.mobs){
-			console.log(main.mobs[i]);
-			if (main.mobs[i].name.toLowerCase() === aggressorName.toLowerCase()){
+		for (let i in game.mobs) {
+			console.log(game.mobs[i]);
+			if (game.mobs[i].name.toLowerCase() === aggressorName.toLowerCase()) {
 				console.log('Found, aggressor assigned.');
-				aggressor = main.mobs[i];
+				aggressor = game.mobs[i];
 				break;
 			}
 		}
 		//find the defender
 		console.log(`Searching for ${defenderName}...`)
-		for(let i in main.mobs){
-			console.log(main.mobs[i]);
-			if (main.mobs[i].name.toLowerCase() === defenderName.toLowerCase()){
+		for (let i in game.mobs) {
+			console.log(game.mobs[i]);
+			if (game.mobs[i].name.toLowerCase() === defenderName.toLowerCase()) {
 				console.log("Found, defender assigned.");
-				defender = main.mobs[i];
+				defender = game.mobs[i];
 				break;
 			}
 		}
 		//TODO: make this sensitive to players.
-		if(aggressor == null){
-			main.mainGameLog(`${aggressorName} was not found, please try again.`);
+		if (aggressor == null) {
+			game.mainGameLog(`${aggressorName} was not found, please try again.`);
 			return false;
 		}
-		if(defender == null){
-			main.mainGameLog(`${defenderName} was not found, please try again.`);
+		if (defender == null) {
+			game.mainGameLog(`${defenderName} was not found, please try again.`);
 			return false;
 		}
-
 		let attackReport = aggressor.attack(defender);
-		if(attackReport == false){
+		if (attackReport == false) {
 			console.log("Attack failed.")
 			return false;
 		}
-		if (!(attackReport[4])){
-			main.mainGameLog(`${attackReport[0]} attacked ${attackReport[2]} dealing ${attackReport[1]} damage, leaving ${attackReport[3]}HP remaining...`);
-			main.giveTurn();
+		if (!(attackReport[4])) {
+			game.mainGameLog(`${attackReport[0]} attacked ${attackReport[2]} dealing ${attackReport[1]} damage, leaving ${attackReport[3]}HP remaining...`);
+			game.giveTurn();
+		} else {
+			game.mainGameLog(`${attackReport[0]} attacked ${attackReport[2]} dealing a killing blow, for ${attackReport[1]} damage!<br>${attackReport[2]} falls over.`);
+			game.reapSouls();
+			game.giveTurn();
+			game.dumDead = true;
 		}
-		else {
-			main.mainGameLog(`${attackReport[0]} attacked ${attackReport[2]} dealing a killing blow, for ${attackReport[1]} damage!<br>${attackReport[2]} falls over.`);
-			main.reapSouls();
-			main.giveTurn();
-			main.dumDead = true;
-		}
-		main.showCombatants();
+		game.showCombatants();
 		console.log(`Attack completed!`)
 		defender.turnTimer--;
 		return attackReport;
 	},
-	controlAI: function(mob){
-		if(mob.cpu){
-			console.log("Computer Player's turn...");
+	controlAI: function(mob) {
+		if (mob.cpu) {
+			console.log("Computer Player's turn!");
 			//TODO: make this more intelligent
 			let hostiles = [];
-			for (let i in main.mobs){
-				if (main.mobs[i].cpu === false){
-					hostiles.push(main.mobs[i]);
+			console.log("Checking for hostiles...")
+			for (let i in game.mobs) {
+				if (game.mobs[i].cpu === false) {
+					console.log("Hostile found.")
+					hostiles.push(game.mobs[i]);
 				}
 			}
-			if (hostiles.length === 0){
-				console.log(`All players deceased. Surrendering turn.`)
-				main.showCombatants();
-				main.changeButtons('visible');
+			if (hostiles.length === 0) {
+				console.log(`All players deceased. Game over.`)
+				game.showCombatants();
+				game.changeButtons('init','visible');
 				return false;
 			}
-			let target = hostiles[Math.floor(Math.random()*hostiles.length)].name;
+			let target = hostiles[Math.floor(Math.random() * hostiles.length)].name;
 			console.log(`Targeting ${target}...`);
-			setTimeout(main.makeAttack,750,mob.name,target);
+			setTimeout(game.makeAttack, 750, mob.name, target);
 			return true;
 		}
-		console.log("Player's turn...");
-		main.changeButtons("visible");
+		console.log("Player's turn!");
+		let hostiles = [];
+		console.log(`Checking for hostiles...`)
+		for (let i in game.mobs) {
+			if (game.mobs[i].cpu === true) {
+				console.log('Hostile found.')
+				hostiles.push(game.mobs[i]);
+			}
+		}
+		if (hostiles.length === 0) {
+			console.log(`No hostiles, battle won!`);
+			game.mainGameLog("Battle won!");
+			game.changeButtons("post","visible");
+		}
+		game.changeButtons("main","visible");
 		return false;
 	},
-	mainGameLog: function(string){
-		main.mainWindow.innerHTML += string + "<br><br>";
-		main.mainWindow.scrollTop = main.mainWindow.scrollHeight;
+	mainGameLog: function(string) {
+		game.mainWindow.innerHTML += string + "<br><br>";
+		game.mainWindow.scrollTop = game.mainWindow.scrollHeight;
 	},
-	showCombatants: function(){
+	showCombatants: function() {
 		console.log(`Displaying combantants...`)
-		main.auxWindow.innerHTML = "";
-		for(let i in main.mobs){
+		game.auxWindow.innerHTML = "";
+		for (let i in game.mobs) {
 			//TODO: make more sensitive to 'teams'
-			if(main.mobs[i].name === main.protag){
-				if(main.mobs[i].name === main.turnOwner){
-					main.auxWindow.innerHTML += `>>${main.mobs[i].name} (You): ${main.mobs[i].hp} HP <br>`;
+			if (game.mobs[i].name === game.protag) {
+				if (game.mobs[i].name === game.turnOwner) {
+					game.auxWindow.innerHTML += `>>${game.mobs[i].name} (You): ${game.mobs[i].hp} HP <br>`;
+				} else {
+					game.auxWindow.innerHTML += `${game.mobs[i].name} (You): ${game.mobs[i].hp} HP <br>`;
 				}
-				else{
-					main.auxWindow.innerHTML += `${main.mobs[i].name} (You): ${main.mobs[i].hp} HP <br>`;
-				}	
-			}
-			else if(main.mobs[i].cpu === false){
-				if(main.mobs[i].name === main.turnOwner){
-					main.auxWindow.innerHTML += `>>${main.mobs[i].name} (Ally): ${main.mobs[i].hp} HP <br>`;
+			} else if (game.mobs[i].cpu === false) {
+				if (game.mobs[i].name === game.turnOwner) {
+					game.auxWindow.innerHTML += `>>${game.mobs[i].name} (Ally): ${game.mobs[i].hp} HP <br>`;
+				} else {
+					game.auxWindow.innerHTML += `${game.mobs[i].name} (Ally): ${game.mobs[i].hp} HP <br>`;
 				}
-				else{
-					main.auxWindow.innerHTML += `${main.mobs[i].name} (Ally): ${main.mobs[i].hp} HP <br>`;
-				}	
-			}
-			else{
-				if(main.mobs[i].name === main.turnOwner){
-					main.auxWindow.innerHTML += `>>${main.mobs[i].name}: ${main.mobs[i].hp} HP <br>`;
-				}
-				else{
-					main.auxWindow.innerHTML += `${main.mobs[i].name}: ${main.mobs[i].hp} HP <br>`;
+			} else {
+				if (game.mobs[i].name === game.turnOwner) {
+					game.auxWindow.innerHTML += `>>${game.mobs[i].name}: ${game.mobs[i].hp} HP <br>`;
+				} else {
+					game.auxWindow.innerHTML += `${game.mobs[i].name}: ${game.mobs[i].hp} HP <br>`;
 				}
 			}
 		}
 	},
-	showTargets: function(){
+	showTargets: function() {
 		console.log('Showing target list!')
 		let buttonHolder = document.querySelector('#buttons');
-		main.changeButtons("hidden");
-		for (let i in main.mobs){
-			console.log(`Creating attack button for: ${main.mobs[i].name}...`)
+		game.changeButtons("main","hidden");
+		game.changeButtons("init","hidden");
+		for (let i in game.mobs) {
+			console.log(`Creating attack button for: ${game.mobs[i].name}...`)
 			let current = document.createElement('button');
-			if (main.mobs[i].name === main.turnOwner){
-				current.innerHTML = `${main.mobs[i].name} (You)`;
-			}
-			else if(main.mobs[i].name === main.protag){
-				current.innerHTML = `${main.mobs[i].name} (Main)`;
-			}
-			else if(main.mobs[i].cpu === false){
-				current.innerHTML = `${main.mobs[i].name} (Ally)`;
-			}
-			else{
-				current.innerHTML = main.mobs[i].name;
+			if (game.mobs[i].name === game.turnOwner) {
+				current.innerHTML = `${game.mobs[i].name} (You)`;
+			} else if (game.mobs[i].name === game.protag) {
+				current.innerHTML = `${game.mobs[i].name} (Main)`;
+			} else if (game.mobs[i].cpu === false) {
+				current.innerHTML = `${game.mobs[i].name} (Ally)`;
+			} else {
+				current.innerHTML = game.mobs[i].name;
 			}
 			current.className = 'temp';
-			let attackThis = function(){
-				main.makeAttack(main.turnOwner,main.mobs[i].name);
-				main.clearTemps();
+			let attackThis = function() {
+				game.makeAttack(game.turnOwner, game.mobs[i].name);
+				game.clearTemps();
 			}
-			current.addEventListener('click',attackThis);
+			current.addEventListener('click', attackThis);
 			buttonHolder.appendChild(current);
 		}
 	},
-
 	/**********************************************
-	*
-	*  Main runtime is here. Nothing else should go 
-	*                 under here.
-	**********************************************/
-	
-	runtime: function (){
-		console.log("main.js loaded successfully.");
-		main.mainGameLog("Please create a character before continuing.");
-		main.createMob("Wummy",100,5,0,0,true);
-		main.createMob("Dummy",100,5,0,0,true);
-		//main.mobs[1].turnTimer = 0;
+	 *
+	 *  Main runtime is here. Nothing else should go 
+	 *                 under here.
+	 **********************************************/
+	runtime: function() {
+		game.changeButtons("main","hidden");
+		game.changeButtons("post","hidden")
+		console.log("game.js loaded successfully.");
+		game.mainGameLog("Please create a character before continuing.");
+		game.createMob("Wummy", 100, 5, 2, 0, true);
+		game.createMob("Dummy", 100, 5, 2, 0, true);
+		//game.mobs[1].turnTimer = 0;
 		//this.makeAttack("Dummy","Wummy");
 	}
 }
